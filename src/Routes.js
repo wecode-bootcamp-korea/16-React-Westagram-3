456bbd5bd58e584ec4364de9bf5caceb5ab407dc
unihit <unihit@naver.com>
2020-12-31 15:01:35 +0900

    Add: 초기세팅 업데이트

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,22 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// eslint-disable-ext-line
 
 // 인태's 컴포넌트
-import LoginIntae from './src/Pages/Intae/Login/Login';
-import MainIntae from './src/Pages/Intae/Main/Main';
+import LoginIntae from "./Pages/Intae/Login/Login";
+import MainIntae from "./Pages/Intae/Main/Main";
 
 // 민선's 컴포넌트
-import LoginMinsun from './src/Pages/Minsun/Login/Login';
-import MainMinsun from './src/Pages/Minsun/Main/Main';
+import LoginMinsun from "./Pages/Minsun/Login/Login";
+import MainMinsun from "./Pages/Minsun/Main/Main";
 
 // 승완's 컴포넌트
-import LoginSeungwan from './src/Pages/Seungwan/Login/Login';
-import MainSeungwan from './src/Pages/Seungwan/Main/Main';
+import LoginSeungwan from "./Pages/Seungwan/Login/Login";
+import MainSeungwan from "./Pages/Seungwan/Main/Main";
 
 // 성현's 컴포넌트
-import LoginSunghyun from './src/Pages/Sunghyun/Login/Login';
-import MainSunghyun from './src/Pages/Sunghyun/Main/Main';
+import LoginSunghyun from "./Pages/Sunghyun/Login/Login";
+import MainSunghyun from "./Pages/Sunghyun/Main/Main";
 
 // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
 class Routes extends React.Component {
@@ -37,7 +34,7 @@ class Routes extends React.Component {
           <Route exact path="/main-sunghyun" component={MainSunghyun} />
         </Switch>
       </Router>
-    )
+    );
   }
 }
 
