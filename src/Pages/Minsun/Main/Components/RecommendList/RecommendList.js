bd5e02cf5b17cfa27b4f16b609315fe7ad64e528
정민선 <minn602@gmail.com>
2021-01-03 20:36:47 +0900

    ADD: 추천리스트 부분 컴포넌트화

---
+++ src/Pages/Minsun/Main/Components/RecommendList/RecommendList.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./RecommendList.scss";
+
+const RECOMMEND_DATA = [
+  {
+    userId: "khloekardashian",
+    imgUrl:
+      "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/129789769_1340573919615966_4744243407009962140_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=QX5DX3GzdmUAX8c6pWF&tp=1&oh=2631991ef8897aca2e5ee7aa26d24c4b&oe=600B12CF",
+  },
+  {
+    userId: "kourtneykardash",
+    imgUrl:
+      "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/129337501_719060642053489_4725676323306299276_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=dxuZe3TvSS0AX_zAtKz&tp=1&oh=777cf9d3c03f5b5f51fd91ae77722a94&oe=6009B283",
+  },
+  {
+    userId: "travisscott",
+    imgUrl:
+      "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/11348214_1481558242162220_192850898_a.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=41L55NhZ9wQAX8BumdO&tp=1&oh=9ca04223d5cb82814d8abd9531bdc78b&oe=600C6C5B",
+  },
+  {
+    userId: "charildamelio",
+    imgUrl:
+      "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/126507807_730870471162439_2370163096545934968_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=K-sboRrMQDQAX_vKcdX&tp=1&oh=25b51c9d414b2cb26f2ae4dee36900ba&oe=600ADD11",
+  },
+  {
+    userId: "addisonaraee",
+    imgUrl:
+      "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/131980604_413741999968339_7651799267812310195_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=43Cli2cp4tIAX_qP_c1&tp=1&oh=24b8c3e24baa42baa785d368786ae45f&oe=600BC46D",
+  },
+];
+
+class RecommendList extends React.Component {
+  render() {
+    return (
+      <ul className="recommendList">
+        {RECOMMEND_DATA.map((data) => {
+          return (
+            <li className="recommendItem">
+              <a href="#">
+                <img className="userImg" src={data.imgUrl} alt={data.userId} />
+              </a>
+              <div className="userInfoContainer">
+                <a className="userId" href="#">
+                  {data.userId}
+                </a>
+                <span className="userInfo">인기</span>
+              </div>
+              <button className="followBtn" type="button">
+                팔로우
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default RecommendList;
--- src/Pages/Minsun/Main/Main.js
+++ src/Pages/Minsun/Main/Main.js
@@ -3,7 +3,8 @@ import "./Main.scss";
 
 import Nav from "../../../Components/Nav/Nav";
 import Lists from "../../../Components/List/Lists";
-import StoryFeed from "./Components/StoryFeed";
+import StoryFeed from "./Components/StoryFeed/StoryFeed";
+import RecommendList from "./Components/RecommendList/RecommendList";
 class MainMinsun extends React.Component {
   constructor() {
     super();
@@ -187,98 +188,7 @@ class MainMinsun extends React.Component {
                     모두 보기
                   </button>
                 </div>
-                <ul className="recomendList">
-                  <li className="recomendItem">
-                    <a href="#">
-                      <img
-                        className="userImg"
-                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/129789769_1340573919615966_4744243407009962140_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=QX5DX3GzdmUAX8c6pWF&tp=1&oh=2631991ef8897aca2e5ee7aa26d24c4b&oe=600B12CF"
-                        alt="khloekardashian"
-                      />
-                    </a>
-                    <div className="userInfoContainer">
-                      <a className="userId" href="#">
-                        khloekardashian
-                      </a>
-                      <span className="userInfo">인기</span>
-                    </div>
-                    <button className="followBtn" type="button">
-                      팔로우
-                    </button>
-                  </li>
-                  <li className="recomendItem">
-                    <a href="#">
-                      <img
-                        className="userImg"
-                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/129337501_719060642053489_4725676323306299276_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=dxuZe3TvSS0AX_zAtKz&tp=1&oh=777cf9d3c03f5b5f51fd91ae77722a94&oe=6009B283"
-                        alt="kourtneykardash"
-                      />
-                    </a>
-                    <div className="userInfoContainer">
-                      <a className="userId" href="#">
-                        kourtneykardash
-                      </a>
-                      <span className="userInfo">인기</span>
-                    </div>
-                    <button className="followBtn" type="button">
-                      팔로우
-                    </button>
-                  </li>
-                  <li className="recomendItem">
-                    <a href="#">
-                      <img
-                        className="userImg"
-                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/11348214_1481558242162220_192850898_a.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=41L55NhZ9wQAX8BumdO&tp=1&oh=9ca04223d5cb82814d8abd9531bdc78b&oe=600C6C5B"
-                        alt="travisscott"
-                      />
-                    </a>
-                    <div className="userInfoContainer">
-                      <a className="userId" href="#">
-                        travisscott
-                      </a>
-                      <span className="userInfo">인기</span>
-                    </div>
-                    <button className="followBtn" type="button">
-                      팔로우
-                    </button>
-                  </li>
-                  <li className="recomendItem">
-                    <a href="#">
-                      <img
-                        className="userImg"
-                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/126507807_730870471162439_2370163096545934968_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=K-sboRrMQDQAX_vKcdX&tp=1&oh=25b51c9d414b2cb26f2ae4dee36900ba&oe=600ADD11"
-                        alt="charildamelio"
-                      />
-                    </a>
-                    <div className="userInfoContainer">
-                      <a className="userId" href="#">
-                        charildamelio
-                      </a>
-                      <span className="userInfo">인기</span>
-                    </div>
-                    <button className="followBtn" type="button">
-                      팔로우
-                    </button>
-                  </li>
-                  <li className="recomendItem">
-                    <a href="#">
-                      <img
-                        className="userImg"
-                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/131980604_413741999968339_7651799267812310195_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=43Cli2cp4tIAX_qP_c1&tp=1&oh=24b8c3e24baa42baa785d368786ae45f&oe=600BC46D"
-                        alt="addisonraee"
-                      />
-                    </a>
-                    <div className="userInfoContainer">
-                      <a className="userId" href="#">
-                        addisonaraee
-                      </a>
-                      <span className="userInfo">인기</span>
-                    </div>
-                    <button className="followBtn" type="button">
-                      팔로우
-                    </button>
-                  </li>
-                </ul>
+                <RecommendList />
               </section>
             </aside>
             <footer>
